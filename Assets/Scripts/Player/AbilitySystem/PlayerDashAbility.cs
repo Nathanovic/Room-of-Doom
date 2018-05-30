43141e05ed4bf95e54bcafa7a80cf232603c5a18
b <bas.de.waal@hotmail.com>
2018-05-30 10:53:55 +0200

    attack melee was weer veranderd

--- Assets/Scripts/Player/AbilitySystem/PlayerDashAbility.cs
+++ Assets/Scripts/Player/AbilitySystem/PlayerDashAbility.cs
@@ -11,14 +11,15 @@ public class PlayerDashAbility : Ability{
     private float dashTime;
     private float curPower;
     private GameObject player;
+    private SpriteRenderer
 
     public override void Init(GameObject p){
         player = p;
-
     }
 
     public override IEnumerator TriggerAbility(){
         Debug.Log("Dash");
+        Ghost();
 
         curPower = power;
         dashTime += Time.deltaTime;
@@ -42,5 +43,10 @@ public class PlayerDashAbility : Ability{
         pb.canControl = true;
         rb.velocity = Vector3.zero;
         dashTime = 0;
+    }
+
+    public void Ghost(){
+
+
     }
 }
