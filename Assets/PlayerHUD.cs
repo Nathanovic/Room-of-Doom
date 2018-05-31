e06891c9e18458e33cd76160d1073ac7a60e9de7
Yamir121 <32955694+Yamir121@users.noreply.github.com>
2018-05-31 09:46:18 +0200

    UI

---
+++ Assets/PlayerHUD.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerHUD : MonoBehaviour {
+
+    public Image healthAmount;
+    public Image fd;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
