3c3da97bf71839a8514b43c56d9ad7a44c05684e
Yamir121 <32955694+Yamir121@users.noreply.github.com>
2018-06-11 16:41:30 +0200

    tweaks

---
+++ Assets/Scripts/SceneSwitch.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class SceneSwitch : MonoBehaviour {
+    public int scene;
+
+    private void Start()
+    {
+        Button buttonComponent = gameObject.GetComponent<Button>();
+    }
+
+    public void Pressed()
+    {
+        SceneManager.LoadScene(scene);
+    }
+}
