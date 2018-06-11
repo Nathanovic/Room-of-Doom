2ddabbbe67a568c7ded66ca6e6f40de6806ea8ac
Nathan <nathanflier@hotmail.com>
2018-06-11 21:21:15 +0200

    rockfall, simple version

--- Assets/Scripts/AI/RockFall.cs
+++ Assets/Scripts/AI/RockFall.cs
@@ -1,16 +1,41 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
 public class RockFall : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-		
+	public GameObject rockPrefab;
+
+	public float minX, maxX, y;
+
+	public int rockAmount = 4;
+	public float minRockFallTime = 0.5f;
+	public float maxRockFallTime = 7f;
+
+	private void Start(){
+		StartSpawning ();
 	}
-	
-	// Update is called once per frame
-	void Update () {
-		
+
+	public void StartSpawning(){
+		for(int i = 0; i < rockAmount; i ++){
+			SpawnRandomNew ();
+		}		
+	}
+
+	private void SpawnRandomNew(){
+		float rndmTime = Random.Range (minRockFallTime, maxRockFallTime);
+		Invoke ("SpawnRock", rndmTime);		
+	}
+
+	private void SpawnRock(){
+		Vector3 rockPos = new Vector3 (Random.Range (minX, maxX), y);
+		GameObject rock = GameObject.Instantiate (rockPrefab, rockPos, Quaternion.identity);
+
+		SpawnRandomNew ();
+	}
+
+	void OnDrawGizmosSelected(){
+		Gizmos.color = Color.grey;
+		Vector3 size = new Vector3 (maxX - minX, 1f, 1f);
+		Vector3 center = new Vector3 (size.x * 0.5f + minX, y, 1f);
+		Gizmos.DrawCube (center, size);
 	}
 }
---
+++ Assets/Scripts/Projectile/Rock.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+
+public class Rock : MonoBehaviour {
+
+	private bool canDoDamage = true;
+
+	public int damage;
+
+	private void Start () {
+	}
+
+	private void OnCollisionEnter2D(Collision2D coll){
+		Collider2D other = coll.collider;
+		Debug.Log ("boulder vs " + other.name + "_candoDamage: " + canDoDamage);
+		if (other.tag == "Player" && canDoDamage) {
+			CharacterCombat combatScript = other.GetComponent<CharacterCombat> ();
+			if (combatScript.ValidTarget ()) {
+				combatScript.ApplyDamage (damage);
+				Destroy (gameObject);
+			}
+		}
+
+		canDoDamage = false;
+		Destroy (gameObject, 0.5f);
+	}
+}
