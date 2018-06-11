3117246008abfc7cc41ac310ea08ac7eb784ea81
Yamir121 <32955694+Yamir121@users.noreply.github.com>
2018-06-11 16:43:12 +0200

    Revert "Merge branch 'master' of https://github.com/UnityGitGit/Room-of-Doom"
    This reverts commit 67d14f6f5239942c8c09bf4e9a47755fda84da43, reversing
    changes made to 3c3da97bf71839a8514b43c56d9ad7a44c05684e.

--- Assets/Scripts/DeadManager.cs
+++ Assets/Scripts/DeadManager.cs
@@ -2,7 +2,6 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
-using UnityEngine.UI;
 
 public class DeadManager : MonoBehaviour {
 
@@ -10,13 +9,7 @@ public class DeadManager : MonoBehaviour {
 
     public int deadCount;
     public int winCount;
-    public int revivedCount;
-    public int lostCount;
-
-    public Text deadAmount;
-    public Text revivesAmount;
-    public Text winsAmount;
-    public Text lostAmount;
+    public int playersRevived;
 
     private int playersAlive;
     private GameObject gameOverScreen;
@@ -31,49 +24,25 @@ public class DeadManager : MonoBehaviour {
         gameOverScreen = transform.GetChild(0).gameObject;
         gameOverScreen.SetActive(false);
 
-        if (Display.displays.Length > 1){
-            Display.displays[1].Activate();
-            Display.displays[1].SetParams(1920, 1080, 0, 0);
-        }
-
-        if (Display.displays.Length > 2){
-            Display.displays[2].Activate();
-            Display.displays[1].SetParams(1920, 1080, 0, 0);
-        }
-
-        lostCount = PlayerPrefs.GetInt("Lost");
-        winCount = PlayerPrefs.GetInt("Win");
-        revivedCount = PlayerPrefs.GetInt("Revived");
-        deadCount = PlayerPrefs.GetInt("Dead");
-
-        deadAmount.text = deadCount.ToString();
-        revivesAmount.text = revivedCount.ToString();
-        winsAmount.text = winCount.ToString();
-        lostAmount.text = lostCount.ToString();
-
     }
 
     private void Update(){
+
         if (gameOverScreen.activeInHierarchy){
             if (Input.GetKeyDown(KeyCode.JoystickButton0)){
                 Restart();
             }
         }
-
-
     }
 
     public void OnPlayerRevive(){
-        revivedCount++;
+        playersRevived++;
         playersAlive++;
-        revivesAmount.text = revivedCount.ToString();
-
     }
 
     public void OnPlayerDead(){
         deadCount++;
         playersAlive--;
-        deadAmount.text = deadCount.ToString();
 
         if (playersAlive == 0){
             GameOver();
@@ -87,28 +56,13 @@ public class DeadManager : MonoBehaviour {
     }
 
     private void GameOver(){
-        lostCount++;
         gameOverScreen.SetActive(true);
-        lostAmount.text = lostCount.ToString();
 
     }
 
-    public void Restart(){
-        PlayerPrefs.SetInt("Lost", lostCount);
-        PlayerPrefs.SetInt("Win", winCount);
-        PlayerPrefs.SetInt("Revived", revivedCount);
-        PlayerPrefs.SetInt("Dead", deadCount);
-
-        SceneManager.LoadScene (SceneManager.GetActiveScene().buildIndex);
+    public void Restart(){ 
+		SceneManager.LoadScene (SceneManager.GetActiveScene().buildIndex);
 
 	}
-
-    [ContextMenu("ResetCount")] 
-    public void ResetCount(){
-        PlayerPrefs.SetInt("Lost", 0);
-        PlayerPrefs.SetInt("Win", 0);
-        PlayerPrefs.SetInt("Revived", 0);
-        PlayerPrefs.SetInt("Dead", 0);
-    }
 
 }
