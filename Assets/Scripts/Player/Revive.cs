7d5326e196105a76d029730a10330a7c6e241fd0
Yamir121 <32955694+Yamir121@users.noreply.github.com>
2018-06-11 14:10:53 +0200

    Revert "bug fix"
    This reverts commit 74f34397465a8fa3e13ec734ab8d3fd58b384620.

--- Assets/Scripts/Player/Revive.cs
+++ Assets/Scripts/Player/Revive.cs
@@ -8,35 +8,28 @@ public class Revive : MonoBehaviour {
     public LayerMask layer;
     public PlayerCombat otherPlayerCombat;
     public PlayerInput.Button buttonToRevive;
-
-    private PlayerCombat playerCombat;
     private PlayerInput playerInput;
     private Animator anim;
 
     private void Start(){
         playerInput = GetComponent<PlayerInput>();
         anim = otherPlayerCombat.gameObject.GetComponentInChildren<Animator>();
-        playerCombat = GetComponent<PlayerCombat>();
+
     }
 
 
     private void Update(){
-        if (playerCombat.health > 0 && (playerInput.ButtonIsDown(buttonToRevive) || Input.GetKeyDown(KeyCode.L))){
-            Collider2D[] hitCollider = Physics2D.OverlapCircleAll(new Vector2(transform.position.x, transform.position.y), radius, layer);
+        if (playerInput.ButtonIsDown(buttonToRevive) || Input.GetKeyDown(KeyCode.L)){
+            Collider2D hitCollider = Physics2D.OverlapCircle(new Vector2(transform.position.x, transform.position.y), radius, layer);
 
-            foreach (var item in hitCollider){
-                Debug.Log(item.name);
-                if (item.transform.root.gameObject == otherPlayerCombat.gameObject){
-                    Debug.Log(item.name);
+            if (hitCollider != null){
+                if (otherPlayerCombat.health <= 0){
+                    otherPlayerCombat.health = 10;
+                    anim.SetBool("dead", false);
+                    
+                    Debug.Log("revived");
+                    otherPlayerCombat.HealthChangedEvent();
 
-                    if (otherPlayerCombat.health <= 0){
-                        otherPlayerCombat.health = 10;
-                        anim.SetBool("dead", false);
-
-                        Debug.Log("revived");
-                        otherPlayerCombat.HealthChangedEvent();
-
-                    }
                 }
             }
         }
