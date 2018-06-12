53cadb87e5c8ecd0b41e996b68cc21a3ae4a8351
b <bas.de.waal@hotmail.com>
2018-06-12 16:15:41 +0200

    popup

--- Assets/Scripts/Combat/PopupDamage.cs
+++ Assets/Scripts/Combat/PopupDamage.cs
@@ -1,17 +1,25 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class PopupDamage : MonoBehaviour {
 
     private Animator ani;
+    private Text damageText;
 
 
     private void Start(){
         ani = transform.GetChild(0).GetComponent<Animator>();
-
+        AnimatorClipInfo[] clipInfos = ani.GetCurrentAnimatorClipInfo(0);
+        Destroy(gameObject, clipInfos[0].clip.length);
+        damageText = ani.GetComponent<Text>();
     }
 
+    public void SetText(string text){
+        damageText.text = text;
 
+
+    }
 
 }
---
+++ Assets/Scripts/PopUpTextManager.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PopUpTextManager : MonoBehaviour {
+
+    private float 
+
+    public static void CreateFloatingText(string tex, Transform pos){
+
+
+    }
+
+
+}
