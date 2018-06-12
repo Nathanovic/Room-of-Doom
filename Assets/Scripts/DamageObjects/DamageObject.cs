d2deae909ecf74ebb43c4ba81eb7ead9615f8f2d
Nathanovich <n.flier@live.nl>
2018-06-12 20:27:31 +0200

    implemented boulder vfx

--- Assets/Scripts/DamageObjects/DamageObject.cs
+++ Assets/Scripts/DamageObjects/DamageObject.cs
@@ -2,18 +2,23 @@
 
 public class DamageObject : MonoBehaviour {
 
+	private ParticleSystem destroyVFX;
+
 	private bool canDoDamage = true;
 	public bool destroyOnCollision;
 
 	public int damage;
+
+	private void Start(){
+		destroyVFX = transform.GetComponentInChildren<ParticleSystem>();
+	}
 
 	private void OnCollisionEnter2D(Collision2D coll){
 		Collider2D other = coll.collider;
 		TryDamageCollider (other);
 
 		if (destroyOnCollision) {
-			canDoDamage = false;
-			Destroy (gameObject, 0.5f);
+			DoImpact ();
 		}
 	}
 
@@ -26,8 +31,19 @@ public class DamageObject : MonoBehaviour {
 			CharacterCombat combatScript = other.GetComponent<CharacterCombat> ();
 			if (combatScript.ValidTarget ()) {
 				combatScript.ApplyDamage (damage);
-				Destroy (gameObject);
+				DoImpact ();
 			}
 		}
+	}
+
+	private void DoImpact(){
+		canDoDamage = false;
+		if (destroyVFX != null) {
+			destroyVFX.Play ();
+			destroyVFX.transform.SetParent (null);
+			Destroy (destroyVFX.gameObject, 1.5f);
+		} 
+
+		Destroy (gameObject);			
 	}
 }
